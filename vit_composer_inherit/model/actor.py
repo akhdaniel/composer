3246e73990cb67636f0a59e208cc4726be6c13d5
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-11-23 19:26:05 +0700

    generate actor

---
+++ vit_composer_inherit/model/actor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from .libs.wavespeed import Wavespeed
+import requests
+import base64
+
+class actor(models.Model):
+
+    _name = "vit.actor"
+    _inherit = "vit.actor"
+
+    def action_generate_actor(self, ):
+        api_key = self.env["ir.config_parameter"].sudo().get_param("wavespeed_api_key")
+        ws = Wavespeed(api_key=api_key)
+        image_url = ws.generate_image(image_prompt=self.image_prompt, model_name='bytedance/seedream-v3')
+        self.image_url = image_url
+        self.download_image()
+
+    def download_image(self, field_name):
+        for rec in self:
+            image_url = self.image_url
+            if not image_url :
+                continue
+
+            try:
+                response = requests.get(image_url, timeout=10)
+                if response.status_code == 200:
+                    # response.content sudah berupa bytes
+                    setattr(rec, field_name, base64.b64encode(response.content))
+                else:
+                    # optional: log error / raise warning
+                    raise UserError(
+                        "Failed to download image from %s, status: %s", image_url, response.status_code
+                    )
+            except Exception as e:
+                # optional: log error
+                raise UserError("Error downloading image from %s: %s", image_url, e)
--- vit_composer_inherit/model/song.py
+++ vit_composer_inherit/model/song.py
@@ -68,38 +68,3 @@ Response HANYA dalam Format data JSON plain text, bukan MD, dan harus seperti in
         self.scene_ids = scene_ids
 
 
-    def generate_actor1(self, ):
-        api_key = self.env["ir.config_parameter"].sudo().get_param("wavespeed_api_key")
-        ws = Wavespeed(api_key=api_key)
-        image_url = ws.generate_image(image_prompt=self.actor1_image_prompt, model_name='bytedance/seedream-v3')
-        self.actor1_image_url = image_url
-        self.download_image("actor1_image")
-
-
-    def generate_actor2(self, ):
-        api_key = self.env["ir.config_parameter"].sudo().get_param("wavespeed_api_key")
-        ws = Wavespeed(api_key=api_key)
-        image_url = ws.generate_image(image_prompt=self.actor2_image_prompt, model_name='bytedance/seedream-v3')
-        self.actor2_image_url = image_url
-        self.download_image("actor2_image")
-
-
-    def download_image(self, field_name):
-        for rec in self:
-            image_url = getattr(rec, f"{field_name}_url")
-            if not image_url :
-                continue
-
-            try:
-                response = requests.get(image_url, timeout=10)
-                if response.status_code == 200:
-                    # response.content sudah berupa bytes
-                    setattr(rec, field_name, base64.b64encode(response.content))
-                else:
-                    # optional: log error / raise warning
-                    raise UserError(
-                        "Failed to download image from %s, status: %s", image_url, response.status_code
-                    )
-            except Exception as e:
-                # optional: log error
-                raise UserError("Error downloading image from %s: %s", image_url, e)
